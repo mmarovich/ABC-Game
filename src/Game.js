ac244b94967b680df673752646a589410ddf74b0
mmarovich <mmarovich@gmail.com>
2017-03-16 22:54:26 -0500

    added next button functionality

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import alphabets from './alphabets.json';
+import classNames from 'classnames';
 
 class Game extends Component {
 	constructor(props) {
@@ -7,12 +8,26 @@ class Game extends Component {
 
 		this.state = {
 			alphabets: alphabets,
-			currentPosition: 0
+			currentPosition: 0,
+			currentTick: 0
+		}
+
+		this.next = this.next.bind(this);
+	}
+
+	next() {
+		console.log('next button click');
+		if(this.state.currentTick < 2) {
+			this.setState({currentTick: this.state.currentTick + 1})
+		} else {
+			this.setState({currentPosition: this.state.currentPosition + 1, currentTick: 0})
 		}
 	}
 	
 	render() {
-		console.log(alphabets);
+		let showImage = this.state.currentTick !== 0 ? true : false;
+		let showWord = this.state.currentTick === 2 ? true : false;
+		console.log(this.state.currentTick, showImage)
 		return(
 			<div className="game">
 				<div className="option">
@@ -24,19 +39,19 @@ class Game extends Component {
 					<div className="buttons">
 						<a className="button prev">Previous</a>
 						<a className="button sound">Play Sound Again</a>
-						<a className="button next">Next</a>
+						<a onClick={this.next} className="button next">Next</a>
 					</div>
 					<div className="fields">
 						<div className="field-block">
 							<div className="left-field">
-								<div className="placeholder-span hide">Click Next to view Image</div>
-								<img className="letter-image" 
+								<div className={classNames('placeholder-span', {hide: showImage})}>Click Next to view Image</div>
+								<img className={classNames('letter-image', {hide: !showImage})} 
 								src={this.state.alphabets[this.state.currentPosition].image}
 								alt={this.state.alphabets[this.state.currentPosition].word} />
 							</div>
 							<div className="right-field">
-								<div className="placeholder-span hide">Click next to view image</div>
-								<div className="word">
+								<div className={classNames('placeholder-span', {hide: showWord})}>Click next to view image</div>
+								<div className={classNames('word', {hide: !showWord})}>
 									{this.state.alphabets[this.state.currentPosition].word.toUpperCase()}
 								</div>								
 							</div>
